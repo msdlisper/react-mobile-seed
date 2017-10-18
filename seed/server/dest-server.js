a6b50b1d4d46017788571d620b33aef080074b59
zhangpeng38 <zhangpeng38@baidu.com>
2017-10-18 20:10:39 +0800

    添加bird3配置以及构建流程

---
+++ seed/server/dest-server.js
@@ -0,0 +1,51 @@
+const webpack = require('webpack');
+const fs = require('fs');
+const del = require('delete');
+const config = require('config');
+const utils = require('utils');
+const htmlPath = config.path.templateProdHtml;
+const htmlContent = fs.readFileSync(htmlPath).toString();
+const fse = require('fs-extra')
+
+
+// 判断是否符合启动条件
+const init = function () {
+    if (utils.isExist(config.path.dllProd)) {
+        start();
+    } else {
+        utils.logs(['error: 请先yarn dll']);
+    }
+}
+
+const start = function () {
+	
+
+    // Remove dest content
+    del.sync(config.path.prod);
+    utils.ensurePath(config.path.prod);
+
+    // 复制dll到prod
+    fse.copy(config.path.dllProd, config.path.prod + '/dll');
+
+    // 模板替换
+    const prodHtml = utils.replaceTemplate(htmlContent, {
+        buildTime: utils.getBuildTime(),
+        envPrefix: 'qa' // #TODO 根据输入参数来指定发版的前缀
+    });
+
+    fse.writeFileSync(config.path.destTargetHtml, prodHtml);
+
+
+
+    // 构建
+    const webpackConfig = require('build/webpack.config.prod');
+    const compiler = webpack(webpackConfig);
+    compiler.run(function (err, stats) {
+        if (err) {
+            utils.logs(['error: ****************** 构建出错 ******************']);
+        }
+        utils.logs(['info: ****************** 构建完成! ******************']);
+    });
+}
+
+init();
---
+++ seed/server/prod-server.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const birdv3 = require('birdv3');
+const config = require('config');
+const utils = require('utils');
+const express = require('express');
+const birdfile = path.resolve('seed/node_modules/config/bird/birdfile');
+const app = express();
+const port = process.argv[2] || 3000;
+
+app.use('/js', express.static(config.path.prod + 'js/'));
+app.use('/dll', express.static(config.path.prod + 'dll/'));
+
+app.all('/*.html', express.static(config.path.prod));
+app.all('*', birdv3(birdfile));
+app.listen(port, function () {
+    utils.logs(['info: Production Server running on http://localhost:' + port]);
+});

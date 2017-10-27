bd29feb79e83e6cc0cd6245a67840220fea47f42
zhangpeng38 <zhangpeng38@baidu.com>
2017-10-27 20:23:00 +0800

    geneate 少了几个文件

--- seed/commander-line/generate.sh
+++ seed/commander-line/generate.sh
@@ -3,9 +3,12 @@
 if [ $1 ]; then
 	project_name=$1
 	mkdir ../$project_name
-	cp -r .* ../$project_name # include .gitignore
+	cp -r ./* ../$project_name 
+
+	# 怎么复制.*? cp .* 会把上一级都复制...然而我的ss挂了, 我得弄下
+	cp -r ./.babelrc ../$project_name 
+	cp -r ./.gitignore ../$project_name 
 	cd ../$project_name
-	rm -rf .git
 else
 	echo "请输入你的项目名称"
 fi
